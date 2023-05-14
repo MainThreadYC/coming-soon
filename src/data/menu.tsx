34c9a6be66c556b1294bb0ce7778d33622dad847
luisantoniocrag <tricubeart@gmail.com>
2023-05-14 01:42:29 -0600

    add GitHub and Conflux Gran Proposal url to header

--- src/data/menu.tsx
+++ src/data/menu.tsx
@@ -1,5 +1,5 @@
 export const menuItems = [
     {name: "Twitter", href: ""},
-    {name: "GitHub", href: ""},
-    {name: "Conflux Grant Proposal 🚀", href: ""}
+    {name: "GitHub", href: "https://github.com/Event-Drop"},
+    {name: "Conflux Grant Proposal 🚀", href: "https://forum.conflux.fun/t/technical-grant-application-event-drop-poap-conflux-portal-tier-1/18547"}
 ]
