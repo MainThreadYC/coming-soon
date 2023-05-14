b85af4064b80dd4f7666532353120244bb06022e
luisantoniocrag <tricubeart@gmail.com>
2023-05-14 11:37:15 -0600

    mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,42 @@
 import { menuItems } from "@/data/menu";
 import Image from "next/image";
 import Link from 'next/link';
+import Hamburger from 'hamburger-react';
+import { useState } from "react";
 
-const Header:React.FC = () =>
-    <div className="absolute top-0 left-0 w-full h-[60px] bg-[#0B002A] flex items-center justify-between px-6 text-white">
-        <Link href="/" id="event-drop-header" className="flex items-center justify-center select-none">
-            <Image src="/event-drop-home.png" alt="Event Drop Icon" width={50} height={50} />
-            <h1 className="ml-1">Event Drop</h1>
-        </Link>
-        <ul className="flex items-center">
-          {menuItems.map((item, index) => 
-            <li className={`${index === menuItems.length-1  ? 'mr-0' : 'mr-8'} hover:opacity-75`} key={item.name}>
-                <a href={item.href} target="_blank">{item.name}</a>
-            </li>)
-          }
-        </ul>
-    </div>
+const Header:React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    return (
+        <div className="absolute top-0 left-0 w-full h-[60px] bg-[#0B002A] flex items-center justify-between px-2 text-white md:px-6">
+            <Link href="/" id="event-drop-header" className="flex items-center justify-center select-none">
+                <Image src="/event-drop-home.png" alt="Event Drop Icon" width={50} height={50} />
+                <h1 className="ml-1">Event Drop</h1>
+            </Link>
+            <ul className="hidden items-center md:flex">
+            {menuItems.map((item, index) => 
+                <li className={`${index === menuItems.length-1  ? 'mr-0' : 'mr-8'} hover:opacity-75`} key={item.name}>
+                    <a href={item.href} target="_blank">{item.name}</a>
+                </li>)
+            }
+            </ul>
+            <div id="mobile-menu" className="md:hidden">
+                    <div className="absolute top-2 right-2 z-20">
+                        <Hamburger toggled={isMenuOpen} toggle={setIsMenuOpen} />
+                    </div>
+                    <div id="mobile-menu-items" className={`absolute opacity-0 top-0 -left-full w-full h-screen bg-[#0B002A] z-10 transition-all ease-in-out ${isMenuOpen ? '-left-0 opacity-90' : ''}`}>
+                        <ul className="w-full p-6 pt-16">
+                        {menuItems.map((item, index) => 
+                            <li className="text-xl mb-12 underline" key={item.name}>
+                                <a href={item.href} target="_blank">{item.name}</a>
+                            </li>)
+                        }
+                        </ul>
+                    </div>
+            </div>
+           
+        </div>
+    )
+}
 
 export default Header;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,12 +16,12 @@ const Main:React.FC = () => {
     }, [email])
 
     return  (
-        <main>
-            <h3 className="p-2 text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FF00E5] via-[#29F2FF] to-[#FFE600]">
+        <main className="flex flex-col justify-center items-center">
+            <h3 className="p-2 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FF00E5] via-[#29F2FF] to-[#FFE600] md:text-8xl">
                 Coming soon
             </h3>
             <form className="mt-12 flex justify-center w-full items-center" onSubmit={onSubmitForm}>
-                <input autoFocus className="bg-[#383D6A] text-white font-normal px-3 py-4 min-w-[350px] rounded-tl-lg rounded-bl-lg focus:ring-offset-0 focus:ring-2 focus:ring-[#5FE2FF] focus:outline-[#5FE2FF]" type="email" placeholder="youremail@email.com" value={email} onChange={onUpdateEmail} />
+                <input autoFocus className="bg-[#383D6A] text-white font-normal px-3 py-4 rounded-tl-lg rounded-bl-lg focus:ring-offset-0 focus:ring-2 focus:ring-[#5FE2FF] focus:outline-[#5FE2FF] md:min-w-[350px]" type="email" placeholder="youremail@email.com" value={email} onChange={onUpdateEmail} />
                 <button disabled={isBtnDisabled} className={`${isBtnDisabled ? 'bg-gray-200 text-gray-400 cursor-not-allowed' : 'font-normal bg-white hover:ring-2 hover:ring-[#5FE2FF] active:scale-105'} px-3 py-4 rounded-tr-lg rounded-br-lg ease-in-out`} type="submit">Notify Me!</button>
             </form>
         </main>
