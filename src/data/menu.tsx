39ff6a29d3f8be9b9d3b72c74a1e681ee34340b2
Luis Antonio Cruz Aguilar <luisantoniocrag@Luiss-Laptop.local>
2023-09-06 23:28:22 -0600

    add twitter link

--- src/data/menu.tsx
+++ src/data/menu.tsx
@@ -1,5 +1,3 @@
 export const menuItems = [
-    {name: "Twitter", href: ""},
-    {name: "GitHub", href: "https://github.com/Event-Drop"},
-    {name: "Conflux Grant Proposal 🚀", href: "https://forum.conflux.fun/t/technical-grant-application-event-drop-poap-conflux-portal-tier-1/18547"}
+    {name: "Twitter", href: "https://twitter.com/event_drop_xyz"},
 ]
